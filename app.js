0245f5aefc6f6f59cae0fea90a264aa2992b45e3
sanel zulic <zullumsan@gmail.com>
2020-12-28 17:34:25 +0100

    Added routes and Post schema

--- app.js
+++ app.js
@@ -1,13 +1,34 @@
 const express = require('express')
-
+const mongoose = require('mongoose');
 const app = express();
-
+require('dotenv/config');
 // NOW YOU HAVE THE ABILITY TO CREATE ROUTES IN A VERY SIMPLE WAY
+
+// Middlewares
+// usualy used for auth
+
+// app.use('/posts', () => {
+//     console.log('Hello from middleware');
+//     return true;
+// });
+
+// IMPORT ROUTES
+
+const postsRoute = require('./routes/posts');
+
+app.use('/posts', postsRoute);
 
 // ROUTES
 
 app.get('/', (req, res) => {
-    res.send('We are on homegit add README.md');
+    res.send('We are on home');
+});
+
+
+// CONNECT TO DB
+
+mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true, useUnifiedTopology: true }, () => {
+    console.log('connected to DB!');
 });
 
 // HOW TO DO WE START LISTENING TO THE SERVER
---
+++ models/Post.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const PostSchema = mongoose.Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    }
+});
